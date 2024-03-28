11bfdbaf8913c67910768d7542d7f15460011da3
Furkan Cetinalp <cetinalp.furkan0@gmail.com>
2024-03-28 14:37:42 +0300

    first commit

--- src/appointment_backend/src/lib.rs
+++ src/appointment_backend/src/lib.rs
@@ -1,4 +1,113 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use serde::Serialize;
+use std::{borrow::Cow, cell::RefCell};
+use uuid::Uuid;
+
+#[derive(CandidType, Deserialize, Clone)]
+#[derive(Default, Debug, PartialEq, Serialize)]
+struct Appointment {
+    name: String,
+    mail: String,
+    day: u8,
+    hour: u8,
+}
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+const MAX_VALUE_SIZE: u32 = 1000;
+
+
+impl Storable for Appointment {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+impl BoundedStorable for Appointment {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static APPOINTMENT_MAP: RefCell<StableBTreeMap<u64, Appointment, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
+}
+
+//UPDATE
+#[ic_cdk_macros::update]
+fn create_appointment( appointment:Appointment) ->bool{
+    let len = APPOINTMENT_MAP.with(|p| p.borrow().len());
+    let data = Appointment{
+        name:appointment.name,
+        mail:appointment.mail,
+        day:appointment.day,
+        hour:appointment.hour,
+    };
+    APPOINTMENT_MAP.with(|p| p.borrow_mut().insert(len,data));
+    return true;
+}
+#[ic_cdk_macros::update]
+fn delete_appointment(mail:String) ->bool{
+    let x  = APPOINTMENT_MAP.with(|p| p.borrow().iter().find(|x| x.1.mail==mail )).clone().unwrap();
+    APPOINTMENT_MAP.with(|p| p.borrow_mut().remove(&x.0));
+    return true;
+}
+
+//GET
+
+#[ic_cdk_macros::query]
+fn get_appointment_by_mail(mail:String) ->Appointment{
+    let x  = APPOINTMENT_MAP.with(|p| p.borrow().iter().find(|x| x.1.mail==mail )).clone().unwrap();
+    return x.1;
+}
+
+
+
+#[ic_cdk_macros::query]
+fn get_all_appointments() -> Option<Vec<Appointment>>{
+    let mut result: Vec<Appointment> = vec![];
+    let s2: Vec<_> = APPOINTMENT_MAP.with(|p| p.borrow().iter().map(|x| x).collect());
+    for item in s2.iter() {
+       result.insert(result.len(),item.1.clone());
+    }
+    if result.len() > 0 {
+        return Some(result);
+    }
+    None
+}
+
+#[ic_cdk_macros::query]
+fn get_appointment_by_day_and_hour(day:u8,hour:u8) ->Option<Appointment>{
+    let x:Option<(u64, Appointment)>  = APPOINTMENT_MAP.with(|p| p.borrow().iter().find(|x| x.1.day==day && x.1.hour==hour ));
+    match x {
+        Some(x) => {
+            return Some(x.1);
+        }
+        None => {
+            return None;
+        }
+    }
+}
+
+#[ic_cdk_macros::query]
+fn get_appointment_list_by_day(day:u8) ->Option<Vec<Appointment>>{
+
+    let request : Vec<_> = APPOINTMENT_MAP.with(|p| p.borrow().iter().filter(|x| x.1.day==day).collect::<Vec<_>>());
+    let mut result: Vec<Appointment> = vec![];
+    for item in request.iter() {
+       result.insert(result.len(),item.1.clone());
+    }
+    if result.len() > 0 {
+        return Some(result);
+    }
+    None
 }
--- src/appointment_frontend/src/App.jsx
+++ src/appointment_frontend/src/App.jsx
@@ -1,31 +1,178 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { appointment_backend } from 'declarations/appointment_backend';
+import React from "react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { Formik, useFormik } from 'formik';
+import { Flex, Box, Heading, FormControl, FormLabel, Input, Button, Alert, AlertIcon,AlertTitle,AlertDescription,useToast} from "@chakra-ui/react";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [num, setNum] = useState(-1)
+  const data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+  const [value, setValue] = useState(new Date());
+  const [showHours,setShowHours] = useState(false);
+  const [selected,setSelected] = useState([]);
+  function onChange(nextValue){
+    setValue(nextValue);
+  }
+  const[veri,setVeri] = useState(null);
+  
+  useEffect(() => {
+    appointment_backend.get_appointment_list_by_day(value.getDate())
+          .then(data => ReservedHours(data))
+          .then(data => setVeri(data))
+          .catch(err => console.error("Custom err: ", err));
+    console.log("selected",selected);
+  },[value])
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    appointment_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+  function ReservedHours(data){
+    var sec = [];
+    console.log("ReservedHoursa girdi",data[0]);
+    data[0]?.map(item => {
+      console.log(item["hour"])
+      sec.push(item["hour"])
+    })
+    setSelected(sec);
+  }
+  console.log("selected hours",selected);
+  console.log("value",value);
+  console.log("value.getdate",value.getDate());
+  function onChange(nextValue){
+    setValue(nextValue);
   }
 
+  const toast = useToast();
+  const toastIdRef = React.useRef();
+  function addToast(result,message) {
+    toastIdRef.current = toast({ description:result ,colorScheme:result=='success' ? 'green':'red',  status:result,title:message})
+  }
+  //make an appointmenT
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+    },
+    
+    onSubmit: (values, bag) => {
+      try {
+        let model = {
+          name:values.name,
+          mail:values.email,
+          day:value.getDate(),
+          hour:num
+        };
+        if(model.day < 0 || model.hour <0){
+          addToast("error","Please select date and hour!!");
+            var timer = setTimeout(function () {
+
+            }, 5000);
+        }
+        appointment_backend.create_appointment(model).then((data) => {
+          console.log("gonderim sonucu",data);
+         
+          if(data ==true){
+            addToast("success","Appointment is scheduled");
+            var timer = setTimeout(function () {
+
+            }, 5000);
+            setSelected([...selected,num]);
+          }
+          else{
+            addToast("error","An error is occurred");
+             ResponseMessage("error");
+          }
+        });
+      }
+      catch (error) {
+        ResponseMessage("error");
+        console.log("an error occurred!!!");
+      }
+    }
+  })
+  function ResponseMessage(response) {
+    const toast = useToast()
+    // Function to show the toast
+    const showToast = () => {
+      toast({
+          title: "Action successful",
+          description: "Your changes have been saved.",
+          status: {response},
+          duration: 5000,
+          isClosable: true,
+          position: "top"
+      });
+  };
+
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+      <div style={{ padding: 20 }}>
+          <Button colorScheme="blue" onClick={showToast}>
+              Show Toast
+          </Button>
+      </div>
+  );
+  }
+  
+  return (
+    <>
+    <div className='container clearfix'>
+      <div>
+      <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={() => setShowHours(true)}
+      />
+      </div>
+      <div>{
+         <div className='buttons'>
+        {data.map(number => (
+          <button 
+          className={`${selected.includes(number) ? 'inactive' : 'active'}  ${number==num ? 'selected-button': ''}`} 
+          key={number}
+          onClick={() => setNum(number)}
+          >{number}:00</button>
+        ))}
+        </div>
+      }</div>
+    </div>
+<Flex align='center' width='full' justifyContent='center'>
+        <Box pt='10'>
+          <Box textAlign='center'>
+            <Heading>Make an Appointment</Heading>
+          </Box>
+          <Box my='5'>
+            {formik.errors.general && (
+              <Alert status='error'>{formik.errors.general}</Alert>
+            )}
+          </Box>
+          <Box my={5} textAlign='left' >
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input name='email' onChange={formik.handleChange} onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
+                ></Input>
+              </FormControl>
+
+              <FormControl mt='4'>
+                <FormLabel>Name</FormLabel>
+                <Input name='name' onChange={formik.handleChange} onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                  isInvalid={formik.touched.name && formik.errors.name}
+
+                ></Input>
+              </FormControl>
+              <Button type='submit' mt='4' width='full'>Create</Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>    </>
   );
 }
 
 export default App;
+
+
+//npm i react-calendar
+//npm install formik
+//npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion
